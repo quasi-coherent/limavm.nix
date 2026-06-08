3c4fcd134d56ebd43d0fed0f3af034b1ac1f6da6
Daniel Donohue <d.michael.donohue@gmail.com>
2026-06-08 14:18:01 -0400

    WIP remove nixpkgs input

--- private/flake.nix
+++ private/flake.nix
@@ -2,5 +2,8 @@
   description = "Private inputs";
   inputs.den.url = "github:denful/den";
   inputs.treefmt-nix.url = "github:numtide/treefmt-nix";
+  inputs.actions-nix.url = "github:nialov/actions.nix";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.nixpkgs-stable.url = "github:NixOS/nixpkgs/nixos-26.05";
   outputs = _: { };
 }
